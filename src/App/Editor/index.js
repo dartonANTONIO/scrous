0fe2b1c8b0f78cd1fea785fa89864fb47d1048c0
Dany Boza <dbxdb@protonmail.com>
2017-08-10 15:44:58 -0500

    init editor

--- src/App/Editor/index.js
+++ src/App/Editor/index.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Editor as Creator, EditorState } from 'draft-js'
 
-const Editor = () => (
-  <div>Editor</div>
-)
+class Editor extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      editorState: EditorState.createEmpty()
+    }
+    this.onChange = editorState => this.setState({editorState})
+  }
+  render(){
+    return(
+      <div className='editor-wrapper'>
+        <div className='editor-header'>editor header</div>
+        <div className='editor'>
+          <Creator editorState={this.state.editorState} onChange={this.onChange}/> 
+        </div>
+      </div>
+    )
+  }
+}
+
 export default Editor
